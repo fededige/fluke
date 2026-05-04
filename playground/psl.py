7e318f45d5def1e656640dc0a80e0c4b10711bf8
fededige <fededigeronimo@gmail.com>
2026-05-04 17:07:20 +0200

    solved bug in psl.py

--- playground/psl.py
+++ playground/psl.py
@@ -2,23 +2,12 @@ import typing
 import torch
 
 from fluke import FlukeENV
-from fluke.config import OptimizerConfigurator
-from fluke.data import FastDataLoader
+from fluke.comm import Message
 from fluke.server import EarlyStopping
 from fluke.utils import clear_cuda_cache
 from playground.clientSL import ClientSL
 from playground.serverSL import ServerSL
 from playground.splitfedv2 import SplitFedV2
-
-from datetime import datetime
-class SimpleLogger:
-    def __init__(self, filepath):
-        self.filepath = filepath
-
-    def log(self, message):
-        timestamp = datetime.now().isoformat()
-        with open(self.filepath, "a", encoding="utf-8") as f:
-            f.write(f"[{timestamp}] {message}\n")
 
 class ClientPSL(ClientSL):
     def start_round(self, current_round: int):
@@ -50,35 +39,14 @@ class ClientPSL(ClientSL):
         self.model.cpu()
         clear_cuda_cache()
 
-        # self.send_model() clients don't need to send the model to the server
         self._check_persistency()
         self._save_to_cache()
 
 class ServerPSL(ServerSL):
-    def __init__(
-            self,
-            model: torch.nn.Module,  # modello server-side
-            client_model: torch.nn.Module,  # modello client-side  (solo nel caso centralized)
-            test_set: FastDataLoader | None,
-            clients: typing.Sequence[ClientSL],
-            optimizer_cfg: OptimizerConfigurator,
-            loss_fn: torch.nn.Module,
-            weighted: bool = False,
-            lr: float = 1.0,
-            clipping: float = 0.0,
-            **kwargs,
-    ):
-        super().__init__(
-            model=model,
-            client_model=client_model,
-            test_set=test_set,
-            clients=clients,
-            optimizer_cfg=optimizer_cfg,
-            weighted=weighted,
-            loss_fn=loss_fn,
-            lr=lr,
-            clipping=clipping,
-            **kwargs,
+
+    def broadcast_model(self, eligible: typing.Sequence[ClientPSL]) -> None:
+        self.channel.broadcast(
+            Message(self.client_model, "client_model", "server", inmemory=None), [c.index for c in eligible]
         )
 
     def end_client_round(self, client_index):
@@ -97,7 +65,6 @@ class PSL(SplitFedV2):
         self.server.broadcast_model(eligible)
 
     def run(self, n_rounds: int, eligible_perc: float, finalize: bool = True, **kwargs) -> None:
-        # logger = SimpleLogger("psl.log") #TODO: remove after tests
         self.server = typing.cast(ServerPSL, self.server)
         with FlukeENV().get_live_renderer():
             progress_sl = FlukeENV().get_progress_bar("FL")
@@ -115,8 +82,6 @@ class PSL(SplitFedV2):
 
                     eligible = typing.cast(typing.Sequence[ClientPSL], self.server.get_eligible_clients(eligible_perc)) # eligible is still of type Sequence[Client] but the type checker knows that the return type is Sequence[ClientPSL]
 
-                    # self.server.broadcast_model(eligible) in PSL broadcast_model is not needed at every round
-
                     self.notify(event="selected_clients", round=rnd + 1, clients=eligible)
 
                     for c, client in enumerate(eligible):
@@ -132,14 +97,8 @@ class PSL(SplitFedV2):
                         progress_client.update(task_id=task_local, completed=c + 1)
                         progress_sl.update(task_id=task_rounds, advance=1)
 
-                    # client_models = self.server.receive_client_models(eligible, state_dict=False)
-                    # self.server.aggregate(eligible, client_models, self.server.client_model) #FedAvg over client models
-                    # self.server.aggregate(eligible, [self.server.per_client_server_models[c.index] for c in eligible], self.server.model) #this aggregation is not needed in SFLV2
-
                     # we choose a random client for evaluation
-                    # logger.log(eligible)
                     random_client = eligible[0]
-                    # logger.log(random_client)
                     random_client.send_model()
                     self.server.receive_client_model(random_client.index)
 
